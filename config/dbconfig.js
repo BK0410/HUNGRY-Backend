498eaa85d2c726221eb55408a852325ac681f1ef
BK0410 <barathkumar04102000@gmail.com>
2022-07-30 13:03:05 +0530

    Backend Initial commit

---
+++ config/dbconfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+    HOST: 'database-main-1.cjguqwdfvh1n.us-east-1.rds.amazonaws.com',
+    USER: 'BKusername',
+    PASSWORD: 'Barath0410',
+    DATABASE: 'project_hungry',
+    DIALECT: 'mysql'
+}
+
